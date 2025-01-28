2a349ec0c8e65cee5a44fc2e14443a71e3eb194b
goose9991 <150225037+goose9991@users.noreply.github.com>
2025-01-28 15:40:02 +0000

    first commit

---
+++ src/MixMatch.java
@@ -0,0 +1,23 @@
+public class MixMatch {
+
+    public static void main(String[] args) {
+        String someString = "Hello";
+        char someChar = 'a';
+        int num = 1;
+        short smallNum = 2;
+        long longNum = 2000;
+        float someFloat = 0.1234567f;
+        double someDouble = 0.123456789;
+        boolean someBoolean = false;
+
+        System.out.println(someString + " <- A String is a non-primitive data type that represents a sequence of character values.");
+        System.out.println(someChar + " <- A char datatype stores a single character/letter or ASCII values.");
+        System.out.println(num + " <- An integer datatype stores whole numbers from -2,147,483,648 to 2,147,483,647.");
+        System.out.println(smallNum + " <- A short datatype stores whole numbers from -32,768 to 32,767.");
+        System.out.println(longNum + " <- A long datatype stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.");
+        System.out.println(someFloat + " <- A float datatype stores fractional numbers. Sufficient for storing 6 to 7 decimal digits.");
+        System.out.println(someDouble + " <- A double datatype stores fractional numbers. Sufficient for storing 15 to 16 decimal digits.");
+        System.out.println(someBoolean + " <- A boolean datatype stores true or false values.");
+
+    }
+}
